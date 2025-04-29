3bd0eae70e8cb3d08444947cf13083baa6b7793f
clement <clementboscher6@gmail.com>
2025-04-29 12:36:13 +0200

    update style

---
+++ pokenative/app/index.tsx
@@ -0,0 +1,21 @@
+import {StyleSheet, Text, View } from "react-native";
+import { Link } from "expo-router";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+export default function Index() {
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+         <Link href="/about">A propos</Link>
+         <Link href={{pathname: "/pokemon/[id]", params: { id: 3}}}>Pokemon 3</Link>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#ff0000"
+  }
+})
+
