c4b5936a790f4fb81d0bf55bd2284e57df569cbc
clement <clementboscher6@gmail.com>
2025-04-29 16:47:06 +0200

    update shadows c

---
+++ pokenative/components/Card.tsx
@@ -0,0 +1,17 @@
+import { Shadows } from "@/constants/Shadows";
+import { useThemeColors } from "@/hooks/useThemeColors";
+import { useTheme } from "@react-navigation/native";
+import {View, type ViewProps, type ViewStyle } from "react-native";
+
+type Props = ViewProps
+
+export function Card ({style, ...rest}: Props) {
+    const Colors = useThemeColors()
+    return <View style={[style, styles, {backgroundColor: Colors.grayWhite}]} {...rest}/>
+}
+
+const styles = {
+    borderRadius: 8,
+    marginTop: 0.5,
+    ...Shadows.dp2,
+}
